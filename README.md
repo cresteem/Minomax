eece58e1f2afe86300c16f19bbe0e217275d031c
DARSAN <iamspdarsan@gmail.com>
2024-06-23 07:56:51 +0530

    Initial setup

---
+++ README.md
@@ -0,0 +1,68 @@
+**Minomax - Comprehensive Web Resource Compressor for Node.js**
+
+Minomax is a powerful Node.js package designed to efficiently compress a wide range of web resources, including videos, images, HTML, CSS, JavaScript, and SVG files. Enhance your website's performance and reduce load times with Minomax's robust compression capabilities.
+
+### Key Features:
+
+- **Video Compression**: Optimize video files for faster streaming and reduced bandwidth usage.
+- **Image Compression**: Compress images without compromising quality, supporting formats like JPEG, PNG, GIF, and more.
+- **HTML Minification**: Reduce the size of your HTML files for quicker page loads.
+- **CSS Minification**: Minimize your CSS files to improve site speed and performance.
+- **JavaScript Minification**: Shrink your JS files to accelerate page rendering.
+- **SVG Compression**: Compress SVG files while maintaining their visual integrity.
+
+### Benefits:
+
+- Boost website performance
+- Decrease load times
+- Reduce bandwidth usage
+- Improve SEO rankings
+
+### Installation:
+
+#### NPM
+
+```bash
+npm install --dev @cresteem/minomax
+```
+
+#### YARN
+
+```bash
+yarn add -D @cresteem/minomax
+```
+
+![GitHub Actions Workflow Status](https://img.shields.io/github/actions/workflow/status/cresteem/Minomax-JS/test.yaml?branch=main&style=flat&logo=github&logoColor=black&logoSize=auto&label=Test&labelColor=white&color=rgb(80%2C200%2C10))
+
+
+Available on NPM:
+
+![NPM Version](https://img.shields.io/npm/v/%40cresteem%2Fminomax?style=flat&logo=rocket&logoColor=black&logoSize=auto&label=Minomax%20Version&labelColor=white)
+
+
+![NPM Downloads](https://img.shields.io/npm/d18m/%40cresteem%2Fminomax?style=flat&logo=rocket&logoColor=black&logoSize=auto&label=Download%20Counts&labelColor=white&color=rgb(80,200,10))
+
+
+##  🧑🏻 Maintainer
+[@iamspdarsan](https://github.com/iamspdarsan) - [LinkedIN](https://in.linkedin.com/in/iamspdarsan) - [Github](https://github.com/iamspdarsan)
+
+## ⚖️ License
+
+Minomax © 2024 by 🚀[CRESTEEM](https://www.cresteem.com/) is licensed under [APACHE LICENSE VERSION 2.0](https://www.apache.org/licenses/LICENSE-2.0.txt)
+
+## 💁🏻 Contribution
+
+By contributing to this project, you agree that your contributions will be licensed under the Apache License, Version 2.0. You can find the full text of the license in the LICENSE file located in the root directory of this project.
+
+For more about contribution check [CONTRIBUTING](./CONTRIBUTING) file.
+
+### ©️ Attribution Requirements
+
+In addition to the terms of the Apache License, Version 2.0, you are required to provide the following attribution in any use, reproduction, and distribution of this software:
+
+"This software includes Minomax © 2024 by [CRESTEEM](https://www.cresteem.com/). Licensed under the Apache License, Version 2.0."
+
+This attribution must be displayed in a prominent location in any documentation, notices, or marketing materials accompanying the distribution or use of this software.
+
+## 🌐 Documenation Website
+https://www.cresteem.com/opensource/minomax
---
+++ lib/options.ts
@@ -0,0 +1,151 @@
+interface mozjpegOptions {
+	quality: number; //0-100
+	color_space: number; //1-grayscale 2-rgb 3-Ycbcr
+	auto_subsample: boolean;
+	chroma_subsample: number; // 1-4
+	progressive: boolean;
+	smoothing: number; //0-100
+	quant_table: number; //quantization: 2-MSSIM kodak ,3-ImageMagick, 4-PSNR kodak
+
+	trellis_multipass: boolean;
+	trellis_opt_zero: boolean; //optimize zero block run
+	trellis_opt_table: boolean; //optimize after trellis
+	trellis_loops: number; //quant passes 1-50
+
+	separate_chroma_quality: boolean;
+	chroma_quality: number; //set only if seperate cq is enabled
+}
+
+interface jpgEncodeOptions {
+	mozjpeg: mozjpegOptions;
+}
+
+interface webpOption {
+	method: number; //effort 0-6
+	quality: number; //0-100
+	alpha_compression: number; // 0 boolean / 1 boolean
+	alpha_quality: number; //0-100
+	alpha_filtering: number; //alpha filter quality 0-2
+	autofilter: number; //auto adjust filter 0 boolean / 1 boolean
+	filter_strength: number; //set if auto filter is 0 (0-100)
+	filter_type: number; //strong filter 0 boolean / 1 boolean
+	filter_sharpness: number; //0-7
+	pass: number; //1-10
+	use_sharp_yuv: number; //sharp RGB->YUV - 1 boolean / 0 boolean
+	sns_strength: number; //spatial noise shaping 0-100
+	preprocessing: number; //0-none / 1-segment smooth / 2- psuedo random dithering
+	segments: number; // 0-4
+	partitions: number; //0-3
+}
+
+interface webpEncodeOptions {
+	webp: webpOption;
+}
+
+interface avifOptions {
+	cqLevel: number; //quality (62-0)
+	subsample: number; //subsample chroma (1 / 0)
+	cqAlphaLevel: number; //seperate alpha quality (62-0) / -1 for disable
+	chromaDeltaQ: boolean; //extra chroma compression (boolean/boolean)
+	sharpness: number; //0-7
+	denoiseLevel: number; //0-50
+	tune: number; //tune 0-auto/1-psnr/2-ssim
+	speed: number; //max effort (10-0)
+}
+
+interface avifEncodeOptions {
+	avif: avifOptions;
+}
+
+//svgo plugin options
+interface svgoPresetDefault {
+	name: "preset-default";
+	params: {
+		overrides: {
+			// customize default plugin options
+			removeDoctype: boolean;
+			removeXMLProcInst: boolean;
+			removeComments: boolean;
+			removeMetadata: boolean;
+			removeEditorsNSData: boolean;
+			cleanupAttrs: boolean;
+			mergeStyles: boolean;
+			inlineStyles: boolean;
+			minifyStyles: boolean;
+			cleanupIds: boolean;
+			removeUselessDefs: boolean;
+			cleanupNumericValues: boolean;
+			convertColors: boolean;
+			removeUnknownsAndDefaults: boolean;
+			removeNonInheritableGroupAttrs: boolean;
+			removeUselessStrokeAndFill: boolean;
+			removeViewBox: boolean;
+			cleanupEnableBackground: boolean;
+			removeHiddenElems: boolean;
+			removeEmptyText: boolean;
+			convertShapeToPath: boolean;
+			moveElemsAttrsToGroup: boolean;
+			moveGroupAttrsToElems: boolean;
+			collapseGroups: boolean;
+			convertPathData: boolean;
+			convertEllipseToCircle: boolean;
+			convertTransform: boolean;
+			removeEmptyAttrs: boolean;
+			removeEmptyContainers: boolean;
+			mergePaths: boolean;
+			removeUnusedNS: boolean;
+			sortAttrs: boolean;
+			sortDefsChildren: boolean;
+			removeTitle: boolean;
+			removeDesc: boolean;
+		}; //overrides end
+		floatPrecision: number;
+		transformPrecision: number;
+		leadingZero: boolean;
+	}; //params end
+}
+
+interface svgoCleanupListOfValues {
+	name: "cleanupListOfValues";
+	params: { floatPrecision: number; leadingZero: boolean };
+}
+
+type svgoPlugings =
+	| svgoPresetDefault
+	| svgoCleanupListOfValues
+	| "removeRasterImages"
+	| "reusePaths"
+	| "removeScriptElement"
+	| "removeOffCanvasPaths"
+	| "removeDimensions";
+
+interface svgOptions {
+	multipass: boolean;
+	plugins: Partial<svgoPlugings>[];
+}
+
+interface imageSetConfigurations {
+	/* 
+	  Image set generator settings.
+	  Sizes are width dependent.
+	  Pixel unit is used in size.
+	  Size always a upperlimit for each set (Example: x1:600) where 600px is upper limit
+	  */
+	screenSizes: {
+		"1X": number;
+		"2X": number;
+		"3X": number;
+		"4X": number;
+		"5X": number;
+	}; //Screen sizes upper-limits
+	upscaleLevel: "l1" | "l2" | "l3";
+	fileSuffix: string;
+}
+
+export interface ConfigurationOptions {
+	jpgEncodeOptions: jpgEncodeOptions;
+	webpEncodeOptions: webpEncodeOptions;
+	avifEncodeOptions: avifEncodeOptions;
+	svgOptions: svgOptions;
+	imageSetConfigurations: imageSetConfigurations;
+}
